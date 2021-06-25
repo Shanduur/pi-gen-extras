3dff47822f2aa4ee29403248bd8da65d5aaced48
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2021-06-25 11:11:07 +0200

    fix(prepare): fixed path and added partition size mod

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-ln -s ./pi-gen/stage2-trim                  ./stage2-trim 
-ln -s ./pi-gen/stage3-containerization      ./stage3-containerization 
-ln -s ./pi-gen/stage3-virtualization        ./stage3-virtualization 
-ln -s ./pi-gen/stage3.5-orchestration       ./stage3.5-orchestration 
-ln -s ./pi-gen/config                       ./config
+HERE=$(pwd)
+
+sed -i 's/4MiB,250MiB,c,*/4MiB,256MiB,c,*/g'    pi-gen/common/qcow2_handling
+sed -i 's/260MiB,,83;/260MiB,,83/g'             pi-gen/common/qcow2_handling
+
+ln -s $HERE/stage2-trim               $HERE/pi-gen/stage2-trim 
+ln -s $HERE/stage3-containerization   $HERE/pi-gen/stage3-containerization 
+ln -s $HERE/stage3-virtualization     $HERE/pi-gen/stage3-virtualization 
+ln -s $HERE/stage3.5-orchestration    $HERE/pi-gen/stage3.5-orchestration 
+ln -s $HERE/config                    $HERE/pi-gen/config
