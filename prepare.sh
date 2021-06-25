c17765a2cc11c83d7eb88a6ff09f3926cee516e8
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2021-06-25 10:52:46 +0200

    fix: added stages to config

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-ln -s ./pi-gen/stage2-trim ./stage2-trim 
-ln -s ./pi-gen/config ./config
+ln -s ./pi-gen/stage2-trim                  ./stage2-trim 
+ln -s ./pi-gen/stage3-containerization      ./stage3-containerization 
+ln -s ./pi-gen/stage3-virtualization        ./stage3-virtualization 
+ln -s ./pi-gen/stage3.5-orchestration       ./stage3.5-orchestration 
+ln -s ./pi-gen/config                       ./config
