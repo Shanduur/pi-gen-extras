9154f43dacbaf5223fc2096eec4693bc9027bea2
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2021-06-25 12:03:05 +0200

    feat: renamed stages

--- prepare.sh
+++ prepare.sh
@@ -7,6 +7,6 @@ sed -i 's/254MiB/260MiB/g'             ./pi-gen/scripts/qcow2_handling
 
 ln -sf $HERE/stage2-trim               $HERE/pi-gen/stage2-trim 
 ln -sf $HERE/stage3-containerization   $HERE/pi-gen/stage3-containerization 
-ln -sf $HERE/stage3-virtualization     $HERE/pi-gen/stage3-virtualization 
-ln -sf $HERE/stage3.5-orchestration    $HERE/pi-gen/stage3.5-orchestration 
+ln -sf $HERE/stage4-virtualization     $HERE/pi-gen/stage4-virtualization 
+ln -sf $HERE/stage5-orchestration    $HERE/pi-gen/stage5-orchestration 
 ln -sf $HERE/config                    $HERE/pi-gen/config
