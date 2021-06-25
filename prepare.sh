6e4d73f278652b19f35ad6873a945b6dcd8debc1
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2021-06-25 11:39:51 +0200

    fix(prepare): fixed linking

--- prepare.sh
+++ prepare.sh
@@ -2,11 +2,11 @@
 
 HERE=$(pwd)
 
-sed -i 's/4MiB,250MiB,c,*/4MiB,256MiB,c,*/g'    pi-gen/common/qcow2_handling
-sed -i 's/260MiB,,83;/260MiB,,83/g'             pi-gen/common/qcow2_handling
+sed -i 's/4MiB,250MiB/4MiB,256MiB/g'    ./pi-gen/scripts/qcow2_handling
+sed -i 's/254MiB/260MiB/g'             ./pi-gen/scripts/qcow2_handling
 
-ln -s $HERE/stage2-trim               $HERE/pi-gen/stage2-trim 
-ln -s $HERE/stage3-containerization   $HERE/pi-gen/stage3-containerization 
-ln -s $HERE/stage3-virtualization     $HERE/pi-gen/stage3-virtualization 
-ln -s $HERE/stage3.5-orchestration    $HERE/pi-gen/stage3.5-orchestration 
-ln -s $HERE/config                    $HERE/pi-gen/config
+ln -sf $HERE/stage2-trim               $HERE/pi-gen/stage2-trim 
+ln -sf $HERE/stage3-containerization   $HERE/pi-gen/stage3-containerization 
+ln -sf $HERE/stage3-virtualization     $HERE/pi-gen/stage3-virtualization 
+ln -sf $HERE/stage3.5-orchestration    $HERE/pi-gen/stage3.5-orchestration 
+ln -sf $HERE/config                    $HERE/pi-gen/config
